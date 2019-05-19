8c5b383609dfb5d58304f7655d472a8fe5a8ddb5
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-19 16:50:38 -0600

    Removes pry.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::ItemsController < ApplicationController
   def index
     business = find_business(look_up_params['api_key'])
     if business == false
-      binding.pry
       unauthorized
     else
       items = Item.distributor_items(business.distributor_id)
