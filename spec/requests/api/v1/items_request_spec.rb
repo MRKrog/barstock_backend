a18004e8dc9e7dafe74bc9dba5893a1b0d0f1f37
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-19 16:50:31 -0600

    Alters expectations for items request, as we altered the format we are expecting the response.

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,44 +2,48 @@ require 'rails_helper'
 
 describe 'Items API', :type => :request do
   context 'with a correct API key' do
-    distributor_1 = Distributor.create!(name: 'RNDC',
-                                      address: '3319 Arapahoe st, Denver, CO',
-                                      distributor_code: 'CODE1234',
-                                      api_key: 'jgn983hy48thw9begh98h4539h4',
-                                      password: 'password'
-                                      )
-    distributor_2 = Distributor.create!(name: 'Other',
-                                      address: 'other',
-                                      distributor_code: 'other',
-                                      api_key: 'other',
-                                      password: 'other'
-                                      )
+    it 'sends a list of items for a successful request' do
+      distributor_1 = Distributor.create!(name: 'RNDC',
+                                        address: '3319 Arapahoe st, Denver, CO',
+                                        distributor_code: 'CODE12345',
+                                        api_key: 'jgn983hy48thw9begh98h4539h41',
+                                        password: 'password'
+                                        )
+      distributor_2 = Distributor.create!(name: 'Other',
+                                        address: 'other',
+                                        distributor_code: 'other',
+                                        api_key: 'other',
+                                        password: 'other'
+                                        )
 
-    business_1 = create(:business, api_key: '111111111', distributor: distributor_1)
-    business_2 = create(:business, api_key: '999999', distributor: distributor_2)
+      business_1 = create(:business, api_key: '111111111', distributor: distributor_1)
+      create(:business, api_key: '999999', distributor: distributor_2)
 
-    item_1 = create(:item, distributor: distributor_1)
-    item_2 = create(:item, distributor: distributor_1)
-    item_3 = create(:item, distributor: distributor_2)
+      item_1 = create(:item, distributor: distributor_1)
+      item_2 = create(:item, distributor: distributor_1)
+      create(:item, distributor: distributor_2)
 
-    it 'sends a list of items for the succesful request' do
+      get '/api/v1/items', params: {'api_key': business_1.api_key}
 
-      get '/api/v1/items', params: {'api_key': 'jgn983hy48thw9begh98h4539h4'}
-
-      result = JSON.parse(response.body)
+      result = JSON.parse(response.body)['data']
 
       expect(response.status).to eq(200)
-      expect(result['data']['attributes']['alc_type'].count).to eq(2)
-      expect(result['data']['attributes']['alc_type'][0]['id']).to eq(item_1.id)
-      expect(result['data']['attributes']['alc_type'][1]['id']).to eq(item_2.id)
-      expect(result['data']['attributes']['alc_type'][0]).to have_key('beer')
-      expect(result['data']['attributes']['alc_type'][0]['beer']['alc_category'][0]['porter']).to have_key('name')
-      expect(result['data']['attributes']['alc_type'][0]['beer']['alc_category'][0]['porter']).to have_key('price')
-      expect(result['data']['attributes']['alc_type'][0]['beer']['alc_category'][0]['porter']).to have_key('ounces')
-      expect(result['data']['attributes']['alc_type'][0]['beer']['alc_category'][0]['porter']).to have_key('unit')
-      expect(result['data']['attributes']['alc_type'][0]['beer']['alc_category'][0]['porter']).to have_key('thumbnail')
-      expect(result['data']['attributes']['alc_type'][1]).to have_key('liquor')
-      expect(result['data']['attributes']['alc_type'][2]).to have_key('wine')
+
+      expect(result.count).to eq(2)
+
+      expect(result[0]['id']).to eq(item_1.id.to_s)
+      expect(result[1]['id']).to eq(item_2.id.to_s)
+
+      expect(result[0]['type']).to eq('item')
+      expect(result[1]['type']).to eq('item')
+
+      expect(result[0]['attributes']['alc_type']).to eq(item_1.alc_type)
+      expect(result[0]['attributes']['alc_category']).to eq(item_1.alc_category)
+      expect(result[0]['attributes']['name']).to eq(item_1.name)
+      expect(result[0]['attributes']['price']).to eq(item_1.price)
+      expect(result[0]['attributes']['ounces']).to eq(item_1.ounces)
+      expect(result[0]['attributes']['unit']).to eq(item_1.unit)
+      expect(result[0]['attributes']['thumbnail']).to eq(item_1.thumbnail)
     end
   end
 
