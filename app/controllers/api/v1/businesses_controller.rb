e5a6fac22cf2936c91f8343ef94d97759fecf40a
Julia Marco <marcootj@gmail.com>
2019-05-19 14:19:14 -0600

    Add sad path for business registration

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -2,11 +2,8 @@ class Api::V1::BusinessesController < ApplicationController
 
   def create
     business = Business.create_new(business_params, code_params[:distributor_code])
-    if business.save
-      render json: { api_key: business.api_key }, status: 201
-    else
-      render body: 'Bad Request', status: 400
-    end
+    business.save!
+    render json: { api_key: business.api_key }, status: 201
   end
 
   private
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def render_unprocessable_entity_response(exception)
+    render json: exception.record.errors, status: :unprocessable_entity
+  end
+
 end
