7584dc19d53b66a75f85a8f39b0021c0b56c8595
Julia Marco <35079289+juliamarco@users.noreply.github.com>
2019-05-19 17:23:37 -0600

    Update application_controller.rb
    I don't think we need this methods anymore :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::API
 
-  def find_business(api_key)
-    business = Business.find_by(api_key: api_key)
-    if business == nil
-      false
-    else
-      business
-    end
-  end
-
-  def unauthorized
-    render json: {}, status: 401
-  end
 end
