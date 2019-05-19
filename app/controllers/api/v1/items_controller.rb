65c6f29caa9dc9b2fe629cf37ef4c02ab8d083e5
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-19 12:39:57 -0600

    Adds comments to items controller.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,9 +3,11 @@ class Api::V1::ItemsController < ApplicationController
   def index
     business = find_business(look_up_params['api_key'])
     if business == false
+      binding.pry
       unauthorized
     else
       items = Item.distributor_items(business.distributor_id)
+      # We need to sort this by type somehow
       render json: ItemSerializer.new(items)
     end
   end
