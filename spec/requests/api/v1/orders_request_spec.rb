edabd125d616e4dff682b4947bfaaba65e5b9afc
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-21 16:35:14 -0600

    Adds sad path testing.

--- spec/requests/api/v1/orders_request_spec.rb
+++ spec/requests/api/v1/orders_request_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe 'Orders API', :type => :request do
   context 'with a correct API key' do
     it 'creates an order for a successful request' do
-
       distributor = Distributor.create!(name: 'RNDC',
                                         address: '3319 Arapahoe st, Denver, CO',
                                         code: 'CODE1234',
@@ -60,19 +59,105 @@ describe 'Orders API', :type => :request do
       expect(OrderItem.all[0].price).to eq(25)
       expect(OrderItem.all[1].price).to eq(50)
     end
+
+    it 'responds with a 422 status for bad request due to missing total_cost and total_revenue' do
+      distributor = Distributor.create!(name: 'RNDC',
+                                        address: '3319 Arapahoe st, Denver, CO',
+                                        code: 'CODE1234',
+                                        api_key: 'jgn983hy48thw9begh98h4539h4',
+                                        password: 'password'
+                                        )
+
+      item_1 = create(:item, distributor: distributor)
+      item_2 = create(:item, distributor: distributor)
+      create(:business, api_key: 'jgn983hy48thw9begh98h4539h4', distributor: distributor)
+
+      params = {'api_key': 'jgn983hy48thw9begh98h4539h4',
+                'items': [{
+                   'id': "#{item_1.id}",
+                   'price': '25'
+                 },
+                 {
+                    'id': "#{item_2.id}",
+                    'quantity': '2',
+                    'price': '50'
+                  }
+               ]
+               }
+
+      post '/api/v1/orders', :params => params
+
+      expect(response.status).to eq(422)
+      expect(JSON.parse(response.body)['total_cost']).to eq(["can't be blank", "is not a number"])
+      expect(JSON.parse(response.body)['total_revenue']).to eq(["can't be blank", "is not a number"])
+    end
+
+    it 'responds with a 422 status for bad request due to missing order_item quantity and price' do
+      distributor = Distributor.create!(name: 'RNDC',
+                                        address: '3319 Arapahoe st, Denver, CO',
+                                        code: 'CODE1234',
+                                        api_key: 'jgn983hy48thw9begh98h4539h4',
+                                        password: 'password'
+                                        )
+
+      item_1 = create(:item, distributor: distributor)
+      item_2 = create(:item, distributor: distributor)
+      create(:business, api_key: 'jgn983hy48thw9begh98h4539h4', distributor: distributor)
+
+      params = {'api_key': 'jgn983hy48thw9begh98h4539h4',
+                'total_cost': '200',
+                'total_revenue': '40',
+                'items': [{
+                   'id': "#{item_1.id}"
+                 },
+                 {
+                    'id': "#{item_2.id}",
+                    'quantity': '2',
+                    'price': '50'
+                  }
+               ]
+               }
+
+      post '/api/v1/orders', :params => params
+      expect(response.status).to eq(422)
+      expect(JSON.parse(response.body)['price']).to eq(["can't be blank", "is not a number"])
+      expect(JSON.parse(response.body)['quantity']).to eq(["can't be blank", "is not a number"])
+    end
   end
 
-  # context 'with an incorrect API key' do
-  #   it 'responds with a 400 status for bad request' do
-  #
-  #     get '/api/v1/items', :params => {'api_key': 'incorrect_key'}
-  #
-  #     expect(response.status).to eq(404)
-  #   end
-  # end
+  context 'with an incorrect API key' do
+    it 'responds with a 404 status' do
+      distributor = Distributor.create!(name: 'RNDC',
+                                        address: '3319 Arapahoe st, Denver, CO',
+                                        code: 'CODE1234',
+                                        api_key: 'jgn983hy48thw9begh98h4539h4',
+                                        password: 'password'
+                                        )
 
-  # Request fails if missing one of the fields
+      item_1 = create(:item, distributor: distributor)
+      item_2 = create(:item, distributor: distributor)
+      create(:business, api_key: 'jgn983hy48thw9begh98h4539h4', distributor: distributor)
 
-  #Make sure to test that an incorrect api key is giving back the response we want
-  # and is not creating an order.
+      params = {'api_key': 'incorrect_key',
+                'total_cost': '200',
+                'total_revenue': '40',
+                'items': [{
+                   'id': "#{item_1.id}",
+                   'quantity': '4',
+                   'price': '25'
+                 },
+                 {
+                    'id': "#{item_2.id}",
+                    'quantity': '2',
+                    'price': '50'
+                  }
+               ]
+               }
+
+      post '/api/v1/orders', :params => params
+
+      expect(response.status).to eq(404)
+      expect(JSON.parse(response.body)['error']).to eq("Couldn't find Business")
+    end
+  end
 end
