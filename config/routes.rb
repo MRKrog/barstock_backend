17e30fc8d7b00b43ffd52acb414d24f519f484ce
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-21 14:17:16 -0600

    Adds orders create route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/items', to: 'items#index'
+      post '/orders', to: 'orders#create'
       namespace :businesses do
         post '/register', to: 'registration#create'
         post '/login', to: 'login#create'
