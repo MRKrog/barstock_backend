f207bd9fe63198b39bd4baaae6799e1b0fa5c5b0
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-21 08:36:54 -0600

    Adds order_request_spec.

---
+++ spec/requests/api/v1/orders_request_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'Orders API', :type => :request do
+  context 'with a correct API key' do
+    it 'creates an order for a successful request' do
+
+      item_1 = create(:item, distributor: distributor_1)
+      item_2 = create(:item, distributor: distributor_1)
+      create(:item, distributor: distributor_2)
+
+      params = {"api_key": "jgn983hy48thw9begh98h4539h4",
+                "total_cost": "200",
+                "total_revenue": "40"
+                "items": [{
+                   "id": "1",
+                   "quantity": "4"
+                 }]
+}
+      post '/api/v1/orders', params: params
+
+      result = JSON.parse(response.body)['data']
+
+      expect(response.status).to eq(200)
+
+    end
+  end
+
+  # context 'with an incorrect API key' do
+  #   it 'responds with a 400 status for bad request' do
+  #
+  #     get '/api/v1/items', :params => {'api_key': 'incorrect_key'}
+  #
+  #     expect(response.status).to eq(404)
+  #   end
+  # end
+
+  # Request fails if missing one of the fields
+end
