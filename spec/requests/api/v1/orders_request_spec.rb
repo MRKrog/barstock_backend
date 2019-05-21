30e92d0b11eda81e6da868468d120e7a160ec877
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-21 14:17:02 -0600

    Adds to orders_request spec.

--- spec/requests/api/v1/orders_request_spec.rb
+++ spec/requests/api/v1/orders_request_spec.rb
@@ -3,19 +3,24 @@ require 'rails_helper'
 describe 'Orders API', :type => :request do
   context 'with a correct API key' do
     it 'creates an order for a successful request' do
+      distributor_1 = Distributor.create!(name: 'RNDC',
+                                        address: '3319 Arapahoe st, Denver, CO',
+                                        code: 'CODE1234',
+                                        api_key: 'jgn983hy48thw9begh98h4539h4',
+                                        password: 'password'
+                                        )
 
       item_1 = create(:item, distributor: distributor_1)
       item_2 = create(:item, distributor: distributor_1)
-      create(:item, distributor: distributor_2)
 
       params = {"api_key": "jgn983hy48thw9begh98h4539h4",
                 "total_cost": "200",
-                "total_revenue": "40"
+                "total_revenue": "40",
                 "items": [{
                    "id": "1",
                    "quantity": "4"
                  }]
-}
+               }
       post '/api/v1/orders', params: params
 
       result = JSON.parse(response.body)['data']
