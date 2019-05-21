285a47b944ddc26bf8e934affdf885c3ebe9fa8c
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-21 16:39:28 -0600

    Refactors create_order_items into an order instance method.

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,4 +10,12 @@ class Order < ApplicationRecord
   validates :total_revenue, presence: true, numericality: {
     only_integer: false
   }
+
+  def create_order_items(order_items_params, order_id)
+    count = 0
+    order_items_params.each do |item|
+      OrderItem.create!(quantity: order_items_params[count][:quantity], price: order_items_params[count][:price], order_id: order_id, item_id: order_items_params[count][:id].to_i, unit: 1)
+      count += 1
+    end
+  end
 end
