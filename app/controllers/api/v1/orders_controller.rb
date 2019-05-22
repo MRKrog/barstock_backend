9ce9c7c57f5a66795581dff9962c7ee0951c7d57
Julia Marco <35079289+juliamarco@users.noreply.github.com>
2019-05-22 15:25:17 -0600

    Updated

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::OrdersController < ApplicationController
 
   def create
-    order = Order.create!(total_cost: order_params[:total_cost],
-                          total_revenue: order_params[:total_revenue],
-                          business: get_business(order_params[:api_key]))
-    order.create_order_items(order_params[:items], order.id)
+    order = Order.create!(total_cost: request_body[:total_cost],
+                          total_revenue: request_body[:total_revenue],
+                          business: get_business(request_body[:api_key]))
+    order.create_order_items(request_body[:items], order.id)
     render json: {}, status: 201
   end
 
   private
-  def order_params
+  def request_body
     params.permit(:api_key, :total_cost, :total_revenue, items: [:id, :quantity, :price])
   end
 end
