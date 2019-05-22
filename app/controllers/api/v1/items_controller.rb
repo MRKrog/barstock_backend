98ef6543f32f2196c3473bc2de883363a91ed21e
Julia Marco <35079289+juliamarco@users.noreply.github.com>
2019-05-22 16:59:32 -0600

    Update items_controller.rb
    Changed indentation for update and create items so is easier to read them

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,13 +8,30 @@ class Api::V1::ItemsController < ApplicationController
 
   def create
     distributor = get_distributor(api_key_param[:api_key])
-    Item.create!(name: item_params[:name], alc_type: item_params[:alc_type], alc_category: item_params[:alc_category], price: item_params[:price], quantity: item_params[:quantity], ounces: item_params[:ounces], unit: item_params[:unit], thumbnail: item_params[:thumbnail], distributor_id: distributor.id)
+    Item.create!(name: item_params[:name],
+                 alc_type: item_params[:alc_type],
+                 alc_category: item_params[:alc_category],
+                 price: item_params[:price],
+                 quantity: item_params[:quantity],
+                 ounces: item_params[:ounces],
+                 unit: item_params[:unit],
+                 thumbnail: item_params[:thumbnail],
+                 distributor_id: distributor.id)
     render json: {}, status: 201
   end
 
   def update
     distributor = get_distributor(api_key_param[:api_key])
-    item = Item.update(item_id_param[:id], name: item_params[:name], alc_type: item_params[:alc_type], alc_category: item_params[:alc_category], price: item_params[:price], quantity: item_params[:quantity], ounces: item_params[:ounces], unit: item_params[:unit], thumbnail: item_params[:thumbnail], distributor_id: distributor.id)
+    item = Item.update(item_id_param[:id],
+                       name: item_params[:name],
+                       alc_type: item_params[:alc_type],
+                       alc_category: item_params[:alc_category],
+                       price: item_params[:price],
+                       quantity: item_params[:quantity],
+                       ounces: item_params[:ounces],
+                       unit: item_params[:unit],
+                       thumbnail: item_params[:thumbnail],
+                       distributor_id: distributor.id)
     render json: ItemSerializer.new(item), status: 200
   end
 
