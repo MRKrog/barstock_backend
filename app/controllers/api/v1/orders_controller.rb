61c4e8d481a36ac540829abf5b422d156e072ec5
Julia Marco <35079289+juliamarco@users.noreply.github.com>
2019-05-22 13:27:59 -0600

    Update orders_controller.rb
    Implemented the changes.

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::OrdersController < ApplicationController
 
   def create
-    business = find_business(order_params[:api_key])
-    order = Order.create!(total_cost: order_params[:total_cost], total_revenue: order_params[:total_revenue], business_id: business.id)
+    order = Order.create!(total_cost: order_params[:total_cost],
+                          total_revenue: order_params[:total_revenue],
+                          business: get_business(order_params[:api_key]))
     order.create_order_items(order_params[:items], order.id)
     render json: {}, status: 201
   end
