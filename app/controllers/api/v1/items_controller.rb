81eb05425dfb62ee6440a66ac36e189693b4938e
Julia Marco <marcootj@gmail.com>
2019-05-23 15:49:51 -0600

    Fix test for ordering now in desc order

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -23,15 +23,8 @@ class Api::V1::ItemsController < ApplicationController
   def update
     distributor = get_distributor(api_key_param[:api_key])
     item = Item.update(item_id_param[:id],
-                       name: item_params[:name],
-                       alc_type: item_params[:alc_type],
-                       alc_category: item_params[:alc_category],
-                       price: item_params[:price],
-                       quantity: item_params[:quantity],
-                       ounces: item_params[:ounces],
-                       unit: item_params[:unit],
-                       thumbnail: item_params[:thumbnail],
-                       distributor_id: distributor.id)
+                       item_params)
+    item.distributor = distributor
     render json: ItemSerializer.new(item), status: 200
   end
 
--- app/models/order_facade.rb
+++ app/models/order_facade.rb
@@ -3,7 +3,6 @@ class OrderFacade
   def self.get_orders(business)
     orders = Order.where(business: business)
                   .order(created_at: :desc)
-                  binding.pry
     orders.map do |order|
       OrderInfo.new(order)
     end
