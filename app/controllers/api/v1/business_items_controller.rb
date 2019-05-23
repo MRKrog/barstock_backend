03fce76a9e9f6ad10e5f276bd60b80296054e7f3
Julia Marco <marcootj@gmail.com>
2019-05-22 21:08:03 -0600

    Add update business item endpoint

--- app/controllers/api/v1/business_items_controller.rb
+++ app/controllers/api/v1/business_items_controller.rb
@@ -11,8 +11,17 @@ class Api::V1::BusinessItemsController < ApplicationController
     render json: {}, status: 201
   end
 
+  def update
+    business = get_business(bi_params[:api_key])
+    bi = BusinessItem.update(bi_params[:id],
+                             price_sold: bi_params[:price_sold],
+                             quantity: bi_params[:quantity],
+                             serving_size: bi_params[:serving_size])
+    render json: BusinessItemSerializer.new(bi)
+  end
+
   private
   def bi_params
-    params.permit(:api_key, :price_sold, :quantity, :serving_size, :item_id)
+    params.permit(:api_key, :price_sold, :quantity, :serving_size, :item_id, :id)
   end
 end
